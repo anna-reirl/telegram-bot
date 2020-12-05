3dad02d7a0b5a6d83a6833f39994aa3ce945d68f
Anna <anna_agabekyan_anna@mail.ru>
2020-12-05 17:34:08 +0400

    feat: API, writing the main handler

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import telebot
+import json
+import requests
 
 TOKEN = '1430747342:AAEvIc85jS3rWwRiQ8Ei8r6d_KUh1eB3pak'
 
@@ -24,5 +26,13 @@ def values(message: telebot.types.Message):
     for key in keys.keys():
         text = '\n'.join((text, key))
     bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text',])
+def convert(message: telebot.types.Message):
+    queto, base, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[queto]}&tsyms={keys[base]}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {queto} в {base} - {total_base}'
+    bot.send_message(message.chat.id, text)
 
 bot.polling()
