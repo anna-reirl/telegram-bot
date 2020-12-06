83bc5bf1855285f0a5f683e49683260ce1817e98
Anna <anna_agabekyan_anna@mail.ru>
2020-12-06 21:02:21 +0400

    fix: Error handling

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ keys = {
     'доллар': 'USD',
 }
 
+class ConvertionException(Exception):
+    pass
 
 @bot.message_handler(commands=['start','help'])
 def echo_test(message: telebot.types.Message):
@@ -29,10 +31,33 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text',])
 def convert(message: telebot.types.Message):
-    queto, base, amount = message.text.split(' ')
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[queto]}&tsyms={keys[base]}')
+     values = message.text.split(' ')
+
+     if len(values) > 3:
+         raise ConvertionException("Слишко много параметров")
+    #queto, base, amount
+     quote, base, amount = values
+
+     if quote == base:
+         raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+     try:
+         queto_ticker = keys[quote]
+     except KeyError:
+         raise ConvertionException(f'Не удалось обработать валюту {quote}')
+     try:
+         base_ticker = keys[base]
+     except KeyError:
+         raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+     try:
+         amount = float(amount)
+     except ValueError:
+         raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[queto_ticker]}&tsyms={keys[base_ticker]}')
     total_base = json.loads(r.content)[keys[base]]
-    text = f'Цена {amount} {queto} в {base} - {total_base}'
+    text = f'Цена {amount} {quote} в {base} - {total_base}'
     bot.send_message(message.chat.id, text)
 
 bot.polling()
