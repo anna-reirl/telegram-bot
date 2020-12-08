15f429d15d48a68a8f762ae43d48d2e13fce085d
Anna <anna_agabekyan_anna@mail.ru>
2020-12-08 21:01:17 +0400

    fix: Refactoring

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 import telebot
-import json
-import requests
-
-TOKEN = '1430747342:AAEvIc85jS3rWwRiQ8Ei8r6d_KUh1eB3pak'
+from config import keys, TOKEN
+from utils import ConvertionException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
-keys = {
-    'биткоин': 'BTC',
-    'эфириум': 'ETH',
-    'доллар': 'USD',
-}
-
-class ConvertionException(Exception):
-    pass
 
 @bot.message_handler(commands=['start','help'])
 def echo_test(message: telebot.types.Message):
@@ -29,34 +19,16 @@ def values(message: telebot.types.Message):
         text = '\n'.join((text, key))
     bot.reply_to(message, text)
 
-@bot.message_handler(content_types=['text',])
+@bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-     values = message.text.split(' ')
+    values = message.text.split(' ')
 
-     if len(values) > 3:
-         raise ConvertionException("Слишко много параметров")
-    #queto, base, amount
-     quote, base, amount = values
+    if len(values) != 3:
+        raise ConvertionException('Слишком много параметров')
 
-     if quote == base:
-         raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+    quote, base, amount = values
+    total_base = CryptoConverter.convert(quote, base, amount)
 
-     try:
-         queto_ticker = keys[quote]
-     except KeyError:
-         raise ConvertionException(f'Не удалось обработать валюту {quote}')
-     try:
-         base_ticker = keys[base]
-     except KeyError:
-         raise ConvertionException(f'Не удалось обработать валюту {base}')
-
-     try:
-         amount = float(amount)
-     except ValueError:
-         raise ConvertionException(f'Не удалось обработать количество {amount}')
-
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[queto_ticker]}&tsyms={keys[base_ticker]}')
-    total_base = json.loads(r.content)[keys[base]]
     text = f'Цена {amount} {quote} в {base} - {total_base}'
     bot.send_message(message.chat.id, text)
 
---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '1430747342:AAEvIc85jS3rWwRiQ8Ei8r6d_KUh1eB3pak'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
